3642bfdfd03d528435f7a37a94298023f25830ee
Konee <104918313+KoneeVibes@users.noreply.github.com>
2025-08-01 17:35:33 +0100

    updating the origin to be without slash

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -3,8 +3,8 @@ const allowedOrigins = [
     "http://localhost:2601",
     "http://localhost:2801",
     "http://localhost:8087",
-    "https://caretimes.netlify.app/",
-    "https://caretimes-admin.onrender.com/",
+    "https://caretimes.netlify.app",
+    "https://caretimes-admin.onrender.com",
 ];
 
 module.exports = allowedOrigins;
