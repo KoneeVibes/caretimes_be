f226108896ce3785971005ace55f13506e1ad05e
Konee <104918313+KoneeVibes@users.noreply.github.com>
2026-01-18 05:36:59 +0100

    i am tired atp

--- config/corsOption.js
+++ config/corsOption.js
@@ -2,11 +2,17 @@ const allowedOrigins = require("./allowedOrigin");
 
 const corsOptions = {
 	origin: (origin, callback) => {
-		if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
+		// if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+		// 	callback(null, true);
+		// } else {
+		// 	callback(new Error("Not allowed by CORS"));
+		// }
+		if (!origin) return callback(null, true);
+		if (allowedOrigins.includes(origin)) {
+			return callback(null, true);
 		}
+		console.error("CORS blocked origin:", origin);
+		return callback(new Error("Not allowed by CORS"));
 	},
 	credentials: true,
 	optionsSuccessStatus: 200,
