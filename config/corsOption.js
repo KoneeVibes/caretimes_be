1d33935203536add1fcf22a009c4239049d2f3a3
Konee <104918313+KoneeVibes@users.noreply.github.com>
2026-01-18 03:31:18 +0100

    Trying out this fix one more time

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,14 +1,25 @@
 const allowedOrigins = require("./allowedOrigin");
 
 const corsOptions = {
+	// origin: (origin, callback) => {
+	// 	if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+	// 		callback(null, true);
+	// 	} else {
+	// 		callback(new Error("Not allowed by CORS"));
+	// 	}
+	// },
 	origin: (origin, callback) => {
-		if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error("Not allowed by CORS"));
+		if (!origin) {
+			return callback(null, true);
 		}
+		if (allowedOrigins.includes(origin)) {
+			return callback(null, true);
+		}
+		callback(new Error("Not allowed by CORS"));
 	},
 	credentials: true,
+	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+	allowedHeaders: ["Content-Type", "Authorization"],
 	optionsSuccessStatus: 200,
 };
 
