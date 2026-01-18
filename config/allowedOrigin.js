8f2cd26f0a0711caa8c0380d8ea2fa138dc72b77
Konee <104918313+KoneeVibes@users.noreply.github.com>
2026-01-18 14:05:16 +0100

    added new route

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,10 +1,11 @@
 const allowedOrigins = [
-    "http://192.168.0.152:2601",
-    "http://localhost:2601",
-    "http://localhost:2801",
-    "http://localhost:8087",
-    "https://caretimes.netlify.app",
-    "https://caretimes-admin.onrender.com",
+	"http://192.168.0.152:2601",
+	"http://localhost:2601",
+	"http://localhost:2801",
+	"http://localhost:8087",
+	"https://caretimes.netlify.app",
+	"https://caretimes-admin.onrender.com",
+	"https://caretimes-web.onrender.com",
 ];
 
 module.exports = allowedOrigins;
